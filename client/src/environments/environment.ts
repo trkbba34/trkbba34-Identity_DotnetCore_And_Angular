0686d022d1bec578f0e438e0bc8be84d77a6300d
trkbba34 <tarekchenafi@gmail.com>
2025-01-19 00:28:57 +0100

    S11

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,7 +1,6 @@
 export const environment = {
   production: true,
-  //apiUrl: 'api/',
-  apiUrl: 'http://localhost:5000/api/',
+  apiUrl: 'api/',
   userKey: 'identity',
   googleClientId:
     '2066032109-mfsf1cs7a0ki15romdlc2t1jggj2emer.apps.googleusercontent.com',
